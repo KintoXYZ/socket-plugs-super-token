2f4a72a28ec8471377bc3fc9fed109882fcf0310
Igor Yalovoy <to@ylv.io>
2025-03-24 15:36:20 -0600

    Remove old config

--- src/constants.ts
+++ src/constants.ts
@@ -2,11 +2,7 @@ import { ChainSlug } from "@socket.tech/dl-core";
 import { Project, Tokens } from "./enums";
 
 export const ChainSlugToProject: { [chainSlug in ChainSlug]?: Project } = {
-  [ChainSlug.AEVO]: Project.AEVO,
-  [ChainSlug.AEVO_TESTNET]: Project.AEVO_TESTNET,
-  [ChainSlug.LYRA_TESTNET]: Project.LYRA_TESTNET,
-  [ChainSlug.LYRA]: Project.LYRA,
-  [ChainSlug.SX_NETWORK_TESTNET]: Project.SX_NETWORK_TESTNET,
+  
 };
 
 export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
